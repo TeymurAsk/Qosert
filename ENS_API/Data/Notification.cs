fff6ec70eb09097c8ce1ad6d06b2003ff98b5e23
TeymurAsk <teymurasgar@gmail.com>
2024-11-10 15:25:58 +0100

    Notification DbSet + template page

---
+++ ENS_API/Data/Notification.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ENS_API.Data
+{
+    public class Notification
+    {
+        [Key]
+        public Guid NotificationId { get; set; }
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; } = string.Empty;
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public bool Status { get; set; } = false;
+    }
+}
---
+++ EmergencyNS/wwwroot/checkbox.js
@@ -0,0 +1,18 @@
+﻿window.initializeCheckboxes = () => {
+    const selectAllCheckbox = document.getElementById("select-all");
+    const rowCheckboxes = document.querySelectorAll(".row-checkbox");
+
+    selectAllCheckbox.addEventListener("change", () => {
+        rowCheckboxes.forEach(checkbox => {
+            checkbox.checked = selectAllCheckbox.checked;
+        });
+    });
+
+    rowCheckboxes.forEach(checkbox => {
+        checkbox.addEventListener("change", () => {
+            selectAllCheckbox.checked = Array.from(rowCheckboxes).every(checkbox => checkbox.checked);
+        });
+    });
+};
+
+
